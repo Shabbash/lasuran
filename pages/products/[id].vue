ab32a83ede37d5328c724201959643421855fb25
omarmatter2 <omarmatter2001@gmail.com>
2025-08-07 05:01:56 +0300

    product detailse

---
+++ pages/products/[id].vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Product Details</h1>
+
+  <div v-if="loading">Loading...</div>
+  <div v-else>
+    <h2>{{ product.data.name }}</h2>
+    <p>{{ product.data.description }}</p>
+  </div>
+
+
+</template>
+
+<script setup lang="ts">
+// Imports
+import { useApi } from '~/composables/useApi'
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+const route = useRoute()
+const productId = route.params.id
+const { data: product, pending: loading } = useApi(
+`products/${productId}`,
+  {
+    method: 'GET',
+  }
+
+)
+
+</script>
